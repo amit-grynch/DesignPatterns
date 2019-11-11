fd00f179434021618ef526bcac53e271ff06d69d
amitg <amitg@192.168.1.8>
2019-11-11 20:18:17 +0530

    Fluent Builder with Recursive generics for Builder

--- DesignPatterns/src/Builder/PersonBuilderPractice.java
+++ DesignPatterns/src/Builder/PersonBuilderPractice.java
@@ -4,9 +4,8 @@ public class PersonBuilderPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		PersonBuilder pb=new PersonBuilder();
-		Person per=pb.buildName("amit").build();
-		//pb.buildName("sharma");
+		EmployeeBuilder pb=new EmployeeBuilder();
+		Person per=pb.buildName("amit").assignPosition("Developer").build();
 		System.out.println(per);
 
 	}
@@ -22,14 +21,28 @@ class Person{
 	}
 }
 
-class PersonBuilder{
-	Person person=new Person();
-	public PersonBuilder buildName(String name) {
+class PersonBuilder<SELF extends PersonBuilder<SELF>>{
+	protected Person person=new Person();
+	public SELF buildName(String name) {
 		person.name=name;
-		return this;
+		return self();
 	}
 	public Person build() {
 		return person;
+	}
+	public SELF self() {
+		return (SELF) this;
+	}
+}
+
+class EmployeeBuilder extends PersonBuilder<EmployeeBuilder>{
+	
+	public EmployeeBuilder  assignPosition(String position) {
+		person.position=position;
+		return self();
+	}
+	public EmployeeBuilder self() {
+		return this;
 	}
 }
 
