17daa55dcecabd27f8b0eb4d5f71579bf4a76702
amitg <amitg@192.168.1.12>
2019-11-16 20:10:38 +0530

    Factory Method For Factory Design Pattern

---
+++ DesignPatterns/src/FactoryDesignPatterns/PointCordinate.java
@@ -0,0 +1,36 @@
+package FactoryDesignPatterns;
+
+public class PointCordinate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point CartisianPoint=Point.CartisianSystem(20, 30);
+		System.out.println("Cartisian System:"+CartisianPoint);
+		Point polarPoint=Point.PolarSystem(10, 20);
+		System.out.println("PolarSystem:"+polarPoint);
+
+	}
+
+}
+
+class Point{
+	double x,y;
+	private Point(double a,double b) {
+		this.x=a;
+		this.y=b;
+	}
+	
+	public static Point  CartisianSystem(double a,double  b) {
+		 return new Point(a,b);
+	}
+	public static Point PolarSystem(double rho,double theta) {
+		return new Point(rho*Math.cos(theta),rho*Math.sin(theta));
+	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+	
+	
+}
